99b3fc6adbc8a2ce5c49ca065ccbd125c77be342
David <66866223+david-prv@users.noreply.github.com>
2025-03-17 17:57:24 +0100

    added dump mode

--- install_dependencies.py
+++ install_dependencies.py
@@ -25,7 +25,7 @@ def install_plugin(plugin_slug: str) -> bool:
     print(f"[+] Installing plugin: {plugin_slug}")
     return run_wp_cli_command(["wp", "plugin", "install", plugin_slug, "--activate"])
 
-def main(repository_path: str, skip_confirmation: bool) -> None:
+def main(repository_path: str, skip_confirmation: bool, dump_results: bool) -> None:
     if not os.path.isdir(repository_path):
         print(f"[!] Error: {repository_path} is not a valid directory.")
         return
@@ -45,13 +45,22 @@ def main(repository_path: str, skip_confirmation: bool) -> None:
                     if confirmation.lower() != "y" and confirmation.lower() != "yes":
                         continue
 
-                if install_plugin(dependency):
+                if not dump_results:
+                    if install_plugin(dependency):
+                        installed_dependencies.add(dependency)
+                        continue
+                else:
+                    print("     => Skipping automatic installation. Please install manually.")
                     installed_dependencies.add(dependency)
                     continue
 
                 failing_dependencies.add(dependency)
 
     print(f"[+] Finished. {len(failing_dependencies)} errors occurred.")
+
+    if dump_results and len(installed_dependencies) > 0:
+        print("[!] The following dependencies need to be installed manually:")
+        print(installed_dependencies)
 
     if len(failing_dependencies) > 0:
         print("[!] The following dependencies caused an error:")
@@ -75,10 +84,18 @@ if __name__ == "__main__":
         default=False,
         help="skip manual user confirmation per plugin."
     )
+    parser.add_argument(
+        "-d",
+        "--dump",
+        action="store_true",
+        default=False,
+        help="skip automatic installation, just dump dependencies to terminal."
+    )
 
     args = parser.parse_args()
 
     main(
         os.path.abspath(args.repository_path),
-        bool(args.yes)
+        bool(args.yes),
+        bool(args.dump)
     )
